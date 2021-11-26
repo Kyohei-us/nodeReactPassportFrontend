1e1706ff8af9f7c114cc7163daea311df2eccf0d
Kyohei-us <kyohei1212000@gmail.com>
2021-11-26 07:50:42 -0800

    Update youtubeLikedVideos.tsx

--- pages/youtubeLikedVideos.tsx
+++ pages/youtubeLikedVideos.tsx
@@ -2,7 +2,13 @@ import useFetchYoutubeLikedVideos from "../hooks/useFetchYoutubeLikedVideos";
 
 const YoutubeLikedVideos = () => {
   const youtubeLikedVideos = useFetchYoutubeLikedVideos();
-  return <>{youtubeLikedVideos ? youtubeLikedVideos.data : "Loading..."}</>;
+  return (
+    <>
+      <div>
+        {youtubeLikedVideos ? youtubeLikedVideos.data.kind : "Loading..."}
+      </div>
+    </>
+  );
 };
 
 export default YoutubeLikedVideos;
