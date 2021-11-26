24bdb9c52bfc35d372d37d5b1cfe5078b593fedc
Kyohei-us <kyohei1212000@gmail.com>
2021-11-26 03:22:40 -0800

    Update useFetchYoutubeLikedVideos.tsx

--- hooks/useFetchYoutubeLikedVideos.tsx
+++ hooks/useFetchYoutubeLikedVideos.tsx
@@ -16,7 +16,6 @@ export default function useFetchYoutubeLikedVideos() {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
         },
       };
       let ret = await axios(options);
