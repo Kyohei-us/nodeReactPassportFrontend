88e9dfd3561713bd5898db0735095483da597324
Kyohei-us <kyohei1212000@gmail.com>
2022-03-23 09:57:43 -0700

    Modify: liked video channel page

--- pages/youtubeLikedVideo.tsx
+++ pages/youtubeLikedVideo.tsx
@@ -22,7 +22,7 @@ const YoutubeLikedVideo = () => {
                     mainBody.data.items.map((ele: any, index: number) => {
                         return <div key={index}>
                             {ele.snippet.title}: 
-                            <iframe id="ytplayer" width="640" height="360"
+                            <iframe width="640" height="360"
                             src={"https://www.youtube.com/embed/" + ele.snippet.id.videoId + "?autoplay=0"}
                             ></iframe>
                         </div>
